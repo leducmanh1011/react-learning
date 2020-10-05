95ea603fa1b70f913673d716973f80ec4b864edb
ManhLD <leducmanh101198@gmail.com>
2020-10-05 13:57:10 +0700

    Functional Components

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// JSX
-// ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
-
-// var myNewP = document.createElement("p")
-// myNewP.innerHTML = "This is a paragraph."
-
-
-ReactDOM.render(
-  <ul>
+function MyApp() {
+  return (
+    <ul>
     <li>1</li>
     <li>2</li>
     <li>3</li>
-  </ul>,
+  </ul>
+  )
+}
+
+ReactDOM.render(
+  <MyApp />,
   document.getElementById("root")
 )
