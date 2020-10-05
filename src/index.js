a70f66ad2cd57450a81f525da9a28c92fa460e44
ManhLD <leducmanh101198@gmail.com>
2020-10-05 14:04:52 +0700

    Move components into separate files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyInfo from './components/MyInfo.js';
 import * as serviceWorker from './serviceWorker';
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Bob Ziroll</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Thailand</li>
-        <li>Japan</li>
-        <li>Nordic Countries</li>
-      </ul>
-    </div>
-  )
-}
+
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
