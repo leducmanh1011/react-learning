deca1c5b38f45248aa7dd634f640bab945d006d2
ManhLD <le.duc.manh@sun-asterisk.com>
2020-10-06 13:58:59 +0700

    Mapping conponents

--- src/App.js
+++ src/App.js
@@ -5,26 +5,24 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import TodoItem from './components/TodoItem'
 import ContactCard from './components/ContactCard';
+import Joke from './components/Joke';
+import jokesData from './components/JokesData';
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findindex
 
 function App() {
+  const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+
   return (
-    <div className="contacts">
-      <ContactCard
-        contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-      />
-
-      <ContactCard
-        contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-      />
-
-      <ContactCard
-        contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-      />
-
-      <ContactCard
-        contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-      />
+    <div>
+      {jokeComponents}
     </div>
   );
 }
